b3ce405462efefe0c134594e291e1dbbfc3a58fb
Rawaho <rawahodev@gmail.com>
2017-07-21 23:08:38 +1200

    Added support for the latest hotfix.

---
+++ sql/updates/authentication/0001_2017_07_21_version.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version` (`file`, `version`, `digest`) VALUES
+    ('ffxiv.exe', 17803344, '7ee17359bd6ecd1fe7d4a03fc8f0f23760a412ac'),
+    ('ffxiv_dx11.exe', 26600016, 'a2e9d228505c37b4b355db251be822e4b0d78026');
