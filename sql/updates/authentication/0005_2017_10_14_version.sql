f0cfa561d177020b7ee5a8d693457126cb412c4f
Rawaho <rawahodev@gmail.com>
2017-10-14 12:54:06 +1300

    [SQL] Added support for the latest build.

---
+++ sql/updates/authentication/0005_2017_10_14_version.sql
@@ -0,0 +1,3 @@
+INSERT INTO `version` (`file`, `version`, `digest`) VALUES
+    ('ffxiv.exe', 18198608, '47353eae96fdd49336433713c7245346e99cb5ef'),
+    ('ffxiv_dx11.exe', 27146832, '3518473d82c642e972f3d575922ea911a85179e8');
